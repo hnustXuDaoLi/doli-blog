13b9c5fc9877732ce6bae2f2175689d4c8d8bcea
doli <709523185@qq,com>
2019-08-05 23:22:56 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    title: 'Doli-Blog',
+    description: 'VuePress demo',
+    serviceWorker: true,
+    head: [
+        ['link', {rel: 'icon', href: `/favicon.ico`}]
+    ],
+    base: '/',
+    themeConfig: {
+        nav: [
+            {text: '主页', link: '/'},
+            // 下拉列表
+            {
+                text: '大前端',
+                items: [
+                    {
+                        text: '基础',
+                        items: [
+                            { text: 'JavaScript', link: '/web/JavaScript/test.md' },
+                            { text: 'HTML', link: '' },
+                        ]
+                    },
+
+
+
+                    {text: 'node', link: '/select/select2'}
+                ]
+            },
+            {text: 'GitHub', link: 'http://liuyukuai.github.io'}
+        ],
+        sidebar: {
+            '/web/': [
+                {
+                    title: 'HTML',
+                    collapsable: true,
+                    children: [
+
+                    ]
+                },
+                {
+                    title: 'JavaScript',
+                    collapsable: true,
+                    children: [
+                        'JavaScript/test',
+                    ]
+                }
+            ]
+        }
+    }
+};
